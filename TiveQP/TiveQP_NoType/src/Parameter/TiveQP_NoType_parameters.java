0723e73d452384a3479fb25d2b704e0f8a897376
UbiPLab <69721628+UbiPLab@users.noreply.github.com>
2022-01-18 18:28:20 +0800

    Update TiveQP_NoType_parameters.java

--- TiveQP/TiveQP_NoType/src/Parameter/TiveQP_NoType_parameters.java
+++ TiveQP/TiveQP_NoType/src/Parameter/TiveQP_NoType_parameters.java
@@ -1,7 +1,7 @@
 package Parameter;
 
 public interface TiveQP_NoType_parameters {
-    String fileName ="./2w_random.txt";
+    String fileName ="./Data/20k_random.txt";
     String treeStorePath="./Cache/";
     
     
