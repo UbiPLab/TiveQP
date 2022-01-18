a5486e4dd27822784e41e671e47194afa30067b3
UbiPLab <69721628+UbiPLab@users.noreply.github.com>
2022-01-18 16:50:38 +0800

    Update IBFTree_parameters.java

--- TiveQP/IBFTree/src/Parameter/IBFTree_parameters.java
+++ TiveQP/IBFTree/src/Parameter/IBFTree_parameters.java
@@ -1,8 +1,8 @@
 package Parameter;
 
 public interface IBFTree_parameters {
-    String fileName ="C:\\Users\\CYF\\Desktop\\Test\\2w_random.txt";
-    String tempPath="C:\\Users\\CYF\\Desktop\\Test\\temp";
+    String fileName ="./2w_random.txt";
+    String treeStorePath="./Cache/";
     //  5km  10 7
     //  4km  13 8
     //  3km  17 9
